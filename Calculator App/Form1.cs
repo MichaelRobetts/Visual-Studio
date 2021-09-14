84aba494d3e09d02d5801ac0695e255788bf47e2
Jose Alberto Nunez <josealbertonb@gmail.com>
2021-09-14 16:50:05 -0400

    Update Form1.cs
    Test 2

--- Calculator App/Form1.cs
+++ Calculator App/Form1.cs
@@ -105,6 +105,7 @@ namespace Calculator_App
         }
         private void minusButton_Click(object sender, EventArgs e)
         {
+            //Another commit
             function = '-';
         }
         private void multiplyButton_Click(object sender, EventArgs e)
